22dcdf5f26e39472dcbf0d2588c0315d2ce6b65d
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-08-14 13:35:11 +0300

    get pairs that sum to zero, the naive approach

--- multiple_pointers/naive_approach.js
+++ multiple_pointers/naive_approach.js
@@ -6,3 +6,18 @@ sumZero([-2,0,1,3]) // undefined
 sumZero([1,2,3]) // undefined
 =============================================================================
  */
+
+// O(n^2)
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3,3]
+console.log(sumZero([-2, 0, 1, 3])); // undefined
+console.log(sumZero([1, 2, 3])); // undefined
