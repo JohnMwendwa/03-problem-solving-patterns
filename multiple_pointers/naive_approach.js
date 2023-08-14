56f510be9eef9406bafae4d962230cf397116d22
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-08-14 13:07:59 +0300

    create file

---
+++ multiple_pointers/naive_approach.js
@@ -0,0 +1,8 @@
+/*=============================================================================
+Write a function called sumZero which accepts a sorted array of integers.The function should find the first pair where the sum is 0. Return an array the includes both values that sum to zero or undefined if a pair does not exist.
+
+sumZero([-3,-2,-1,0,1,2,3]) // [-3,3]
+sumZero([-2,0,1,3]) // undefined
+sumZero([1,2,3]) // undefined
+=============================================================================
+ */
