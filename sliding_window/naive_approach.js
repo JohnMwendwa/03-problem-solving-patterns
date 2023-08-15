c0616edd7126805ebba32e19870a07a9d9223396
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-08-15 22:59:42 +0300

    create file

---
+++ sliding_window/naive_approach.js
@@ -0,0 +1,11 @@
+/*
+
+Write a function called maxSubarraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in the array.
+
+maxSubarraySum([1,2,5,2,8,1,5],2) // 10
+maxSubarraySum([1,2,5,2,8,1,5],4) // 17
+maxSubarraySum([4,2,1,6],1) // 6
+maxSubarraySum([4,2,1,6,2],4) // 13
+maxSubarraySum([],4) // null
+
+*/
