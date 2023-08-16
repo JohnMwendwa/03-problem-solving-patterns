48ec314e4a8270dbb65994a3b961077a3a82ac5b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-08-16 23:57:27 +0300

    naive solution for sliding window pattern

--- sliding_window/naive_approach.js
+++ sliding_window/naive_approach.js
@@ -9,3 +9,26 @@ maxSubarraySum([4,2,1,6,2],4) // 13
 maxSubarraySum([],4) // null
 
 */
+
+function maxSubarraySum(arr, num) {
+  if (num > arr.length) return null;
+
+  let max = -Infinity;
+
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    temp = 0;
+    for (let j = 0; j < num; j++) {
+      temp += arr[i + j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  return max;
+}
+
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)); //10
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4)); //17
+console.log(maxSubarraySum([4, 2, 1, 6], 1)); //6
+console.log(maxSubarraySum([4, 2, 1, 6, 2], 4)); //13
+console.log(maxSubarraySum([], 4)); //null
